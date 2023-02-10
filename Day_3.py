f0985a77137308ac044b2fb344615c98f3bae194
Tanimu Abdullahi <aaltanim@gmail.com>
2023-02-10 16:11:31 +0100

    new update

---
+++ Day_3.py
@@ -0,0 +1,32 @@
+# Exercises Day 3
+my_age=36
+my_hight=12.4
+complex_num=1+1j
+
+base=int(input('enter base :'))
+height=int(input('enter height :'))
+area=0.5*base*height
+print('the area of triangle is ',area)
+
+side_a=int(input('enter side a :'))
+side_b=int(input('enter side b :'))
+side_c=int(input('enter side c :'))
+perimeter=side_a+side_b+side_c
+print('the perimeter of the triangle is', perimeter)
+
+length=int(input('enter the length of the rectangle :'))
+width=int(input('enter the width of the rectangle :'))
+area=length*width
+print('the area of the rectangle is' , area)
+perimeter=2*(length+width)
+print('the area of the perimetre is' , perimeter)
+
+import math
+pi=math.pi
+radius=int(input('enter the radius :'))
+area=pi*radius*radius
+print('the area is :', area)
+c=2*pi*radius
+print('the acircumference is :', c)
+
+# question 
---
+++ Day_4.py
@@ -0,0 +1,13 @@
+# EXCERCIESES DAY 4
+one,space,two,four,five='Thirty', ' ', ' Days', 'Of', 'Python' 
+sentence=one+space+two+space+four+space+five
+print(sentence)
+a,b,c='Coding', 'For' , 'All'
+company=a+space+b+space+c
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower()) # capitalize(), title(), swapcase()
+print(company.capitalize())
+print(company.swapcase())
+print(company.title())
