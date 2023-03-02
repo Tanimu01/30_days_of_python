eaac65b4433ef53bc7420b1b2be0a683e858d345
Tanimu Abdullahi <aaltanim@gmail.com>
2023-03-02 09:47:48 +0100

    new

---
+++ Day_14.py
@@ -0,0 +1,6 @@
+# Q1 Explain the difference between map, filter, and reduce.
+# Map: returns an array of pieces of information from the original array. In the callback function, return the data you wish to be part of the new array.
+# Filter: returns a subset of the original array based on custom criteria. In your callback function, return a boolean value to determine whether or not each item will be included in the new array.
+# Reduce: can be used to return almost anything. It is often used to return a single number, like an sum, but it can also be used to combine the logic of Map and Filter to return an array of values matching certain criteria. This can remove unnecessary iterations.
+ 
+# Q2 Explain the difference between higher order function, closure and decorator
