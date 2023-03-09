2a87989843070d7f161eb48e8e632268b45dc8c9
Tanimu Abdullahi <aaltanim@gmail.com>
2023-03-09 14:46:06 +0100

    Statistics

---
+++ Day_24.py
@@ -0,0 +1,190 @@
+import numpy as np
+    # How to check the version of the numpy package
+print('numpy:', np.__version__)
+    # Checking the available methods
+print(dir(np))
+# Creating python List
+python_list = [1,2,3,4,5]
+
+    # Checking data types
+print('Type:', type (python_list)) # <class 'list'>
+    #
+print(python_list) # [1, 2, 3, 4, 5]
+
+two_dimensional_list = [[0,1,2], [3,4,5], [6,7,8]]
+
+print(two_dimensional_list)  # [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+    # Creating Numpy(Numerical Python) array from python list
+
+numpy_array_from_list = np.array(python_list)
+print(type (numpy_array_from_list))   # <class 'numpy.ndarray'>
+print(numpy_array_from_list) # array([1, 2, 3, 4, 5])
+numy_array_from_list2 = np.array(python_list, dtype=float)
+print(numy_array_from_list2) # array([1., 2., 3., 4., 5.])
+numpy_bool_array = np.array([0, 1, -1, 0, 0], dtype=bool)
+print(numpy_bool_array) # array([False,  True,  True, False, False])
+two_dimensional_list = [[0,1,2], [3,4,5], [6,7,8]]
+numpy_two_dimensional_list = np.array(two_dimensional_list)
+print(type (numpy_two_dimensional_list))
+print(numpy_two_dimensional_list)
+# We can always convert an array back to a python list using tolist().
+np_to_list = numpy_array_from_list.tolist()
+print(type (np_to_list))
+print('one dimensional array:', np_to_list)
+print('two dimensional array: ', numpy_two_dimensional_list.tolist())
+nums = np.array([1, 2, 3, 4, 5])
+print(nums)
+print('shape of nums: ', nums.shape)
+print(numpy_two_dimensional_list)
+print('shape of numpy_two_dimensional_list: ', numpy_two_dimensional_list.shape)
+three_by_four_array = np.array([[0, 1, 2, 3],
+        [4,5,6,7],
+        [8,9,10, 11]])
+print(three_by_four_array.shape)
+
+int_lists = [-3, -2, -1, 0, 1, 2,3]
+int_array = np.array(int_lists)
+float_array = np.array(int_lists, dtype=float)
+
+print(int_array)
+print(int_array.dtype)
+print(float_array)
+print(float_array.dtype)
+
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+two_dimensional_list = np.array([[0, 1, 2],
+                              [3, 4, 5],
+                              [6, 7, 8]])
+
+print('The size:', numpy_array_from_list.size) # 5
+print('The size:', two_dimensional_list.size)  # 3
+
+# Mathematical Operation
+# Addition
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_plus_original = numpy_array_from_list  + 10
+print(ten_plus_original)
+
+# Subtraction
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_minus_original = numpy_array_from_list  - 10
+print(ten_minus_original)
+
+# Multiplication
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_times_original = numpy_array_from_list * 10
+print(ten_times_original)
+
+# Division
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_times_original = numpy_array_from_list / 10
+print(ten_times_original)
+
+# Modulus; Finding the remainder
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_times_original = numpy_array_from_list % 3
+print(ten_times_original)
+
+# Floor division: the division result without the remainder
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_times_original = numpy_array_from_list // 10
+print(ten_times_original)
+
+# Exponential is finding some number the power of another:
+numpy_array_from_list = np.array([1, 2, 3, 4, 5])
+print('original array: ', numpy_array_from_list)
+ten_times_original = numpy_array_from_list  ** 2
+print(ten_times_original)
+
+#Int,  Float numbers
+numpy_int_arr = np.array([1,2,3,4])
+numpy_float_arr = np.array([1.1, 2.0,3.2])
+numpy_bool_arr = np.array([-3, -2, 0, 1,2,3], dtype='bool')
+
+print(numpy_int_arr.dtype)
+print(numpy_float_arr.dtype)
+print(numpy_bool_arr.dtype)
+
+# 2 Dimension Array
+two_dimension_array = np.array([[1,2,3],[4,5,6], [7,8,9]])
+first_row = two_dimension_array[0]
+second_row = two_dimension_array[1]
+third_row = two_dimension_array[2]
+print('First row:', first_row)
+print('Second row:', second_row)
+print('Third row: ', third_row)
+
+two_dimension_array = np.array([[1,2,3],[4,5,6], [7,8,9]])
+first_two_rows_and_columns = two_dimension_array[0:2, 0:2]
+print(first_two_rows_and_columns)
+
+
+# Numpy Zeroes
+    # numpy.zeros(shape, dtype=float, order='C')
+numpy_zeroes = np.zeros((3,3),dtype=int,order='C')
+print(numpy_zeroes)
+numpy_ones = np.ones((3,3),dtype=int,order='C')
+print(numpy_ones)
+# Reshape
+# numpy.reshape(), numpy.flatten()
+first_shape  = np.array([(1,2,3), (4,5,6)])
+print(first_shape)
+reshaped = first_shape.reshape(3,2)
+print(reshaped)
+
+four_by_four_matrix = np.matrix(np.ones((4,4), dtype=float))
+print(four_by_four_matrix)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+normal_array = np.random.normal(79, 15, 80)
+sns.set()
+plt.hist(normal_array, color="grey", bins=50)
+
+np_normal_dis = np.random.normal(5, 0.5, 100)
+np_normal_dis
+## min, max, mean, median, sd
+print('min: ', two_dimension_array.min())
+print('max: ', two_dimension_array.max())
+print('mean: ',two_dimension_array.mean())
+# print('median: ', two_dimension_array.median())
+print('sd: ', two_dimension_array.std())
+
+from scipy import stats
+np_normal_dis = np.random.normal(5, 0.5, 1000) # mean, standard deviation, number of samples
+np_normal_dis
+## min, max, mean, median, sd
+print('min: ', np.min(np_normal_dis))
+print('max: ', np.max(np_normal_dis))
+print('mean: ', np.mean(np_normal_dis))
+print('median: ', np.median(np_normal_dis))
+print('mode: ', stats.mode(np_normal_dis))
+print('sd: ', np.std(np_normal_dis))
+
+plt.hist(np_normal_dis, color="grey", bins=21)
+plt.show()
+temp = np.array([1,2,3,4,5])
+pressure = temp * 2 + 5
+
+plt.plot(temp,pressure)
+plt.xlabel('Temperature in oC')
+plt.ylabel('Pressure in atm')
+plt.title('Temperature vs Pressure')
+plt.xticks(np.arange(0, 6, step=0.5))
+plt.show()
+
+mu = 28
+sigma = 15
+samples = 100000
+
+x = np.random.normal(mu, sigma, samples)
+ax = sns.distplot(x);
+ax.set(xlabel="x", ylabel='y')
+plt.show()
